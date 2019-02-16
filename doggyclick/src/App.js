8c1ac5672be65c2b423a436ad6994eadf6c5d7a3
kallistahalim <kallistahalim@gmail.com>
2019-02-15 22:46:11 -0800

    navbar is goooodd

---
+++ doggyclick/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./components/navbar/index"
+
+function App() {
+    return(
+        <div>
+        <NavBar />
+        </div>
+    )
+}
+
+export default App;
